3f22271a493b28ffd6b3c779b35ea9dbcc562929
Przemyslaw Szafulski <p.szafulski@gmail.com>
2019-03-05 14:53:59 +0000

    Add first passing test to JsonManager

---
+++ LoginScreenTDD/Managers/JsonManager.swift
@@ -0,0 +1,16 @@
+//
+//  JsonManager.swift
+//  LoginScreenTDD
+//
+//  Created by Przemyslaw Szafulski on 05/03/2019.
+//  Copyright © 2019 Przemyslaw Szafulski. All rights reserved.
+//
+
+import Foundation
+
+class JsonManager {
+    
+    func parse(data: Data) -> [String: Any]? {
+        return nil
+    }
+}
--- LoginScreenTDDTests/Managers/JsonManagerTest.swift
+++ LoginScreenTDDTests/Managers/JsonManagerTest.swift
@@ -1,5 +1,5 @@
 //
-//  LoginScreenTDDTests.swift
+//  JsonManagerTest.swift
 //  LoginScreenTDDTests
 //
 //  Created by Przemyslaw Szafulski on 05/03/2019.
@@ -8,8 +8,7 @@
 
 import XCTest
 @testable import LoginScreenTDD
-
-class LoginScreenTDDTests: XCTestCase {
+class JsonManagerTest: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,16 +18,10 @@ class LoginScreenTDDTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testWithEmptyData() {
+        let jsonManager = JsonManager()
+        let result = jsonManager.parse(data: Data())
+        XCTAssertNil(result)
     }
 
 }
